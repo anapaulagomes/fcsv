364cc554ce56c318d764bd281d8ce08ae5da9a72
Ana Paula Gomes <apgomes88@gmail.com>
2018-07-11 23:07:11 +0200

    TA ESCREVENDO MANO

--- fcsv.c
+++ fcsv.c
@@ -5,8 +5,56 @@
  * Write a beautiful CSV
  */
 static PyObject *
-create (PyObject *self)
+create (PyObject *self, PyObject *args)
 {
+    FILE *fp;
+    fp=fopen("filename2.csv","w+");
+    
+    PyObject* list_obj;
+    PyObject* sequence;
+    // PyObject* item; // row
+    PyObject* first_row;
+
+    if (!PyArg_ParseTuple(args, "O", &list_obj)) {
+        return NULL;
+    }
+
+    sequence = PySequence_Fast(list_obj, "Expected a list object");
+    // lista aqui
+    int seq_size = PySequence_Size(list_obj);
+
+    printf("-----======= %d\n", seq_size);
+
+    first_row = PySequence_Fast_GET_ITEM(sequence, 0); // pega o primeiro dict
+    PyObject* header = PyDict_Keys(first_row); // pega as keys desse dict
+    int key_count = PyList_Size(header); // SO FAR SO GOOD
+
+    // data = [{'a': 'lalala'}]
+
+    // loop on the keys and get the values
+    for(int i = 0; i < key_count; i++) { // percorre as keys do dict -> 'id', 'name'
+        PyObject* column = PyList_GetItem(header, i);
+        printf("-----======= %d\n", seq_size);
+        if(PyUnicode_Check(column)) {
+            PyObject* columnAscii = PyUnicode_AsASCIIString(column);
+            printf("-> %s", PyBytes_AsString(columnAscii));
+            fprintf(fp, PyBytes_AsString(columnAscii));
+            Py_DECREF(columnAscii);
+        }
+    }
+    fprintf(fp, "\n");
+    
+
+    Py_DECREF(sequence);
+    Py_DECREF(list_obj);
+    Py_DECREF(first_row);
+    Py_DECREF(header);
+
+    // fp = fopen("filename.csv","w+");
+    // fprintf(fp, "Student Id, Physics, Chemistry, Maths\n");
+    // fprintf(fp, "a,b,c,d");
+    fclose(fp);
+
     return Py_BuildValue("s", "Hello Pythonista");
 }
 
@@ -16,7 +64,7 @@ static char create_docstring[] = "Creating faster CSV using Python but written i
  * List of module methods
  */
 static PyMethodDef module_methods[] = {
-    {"create", (PyCFunction) create, METH_NOARGS, create_docstring},
+    {"create", (PyCFunction) create, METH_VARARGS, create_docstring},
     {NULL}
 };
 
@@ -24,7 +72,7 @@ static struct PyModuleDef fcsv =
 {
     PyModuleDef_HEAD_INIT,
     "fcsv",
-    "usage: fcsv.create()\n",
+    "usage: fcsv.create(filename, )\n",
     -1,   /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
     module_methods
 };
--- writingcsv.py
+++ writingcsv.py
@@ -1,4 +1,15 @@
 import fcsv
 
 
-print(fcsv.create())
+data = [{'paula': 'aaa1', 'b': 'bbbb'}, {'a2': 'aaa2'}, {'a': 'aaa3'}]
+
+# def create(data):  # lista de dicionarios
+#     header = data[0].keys()
+#     values = [row.values() for row in data]
+
+#     return header, values
+
+# header, values = create(data)
+# fcsv.create(header, values)
+
+fcsv.create(data)
